8a613018b36dd0a2f1f602d2e27c84f44dee455e
Bryan Verble <bryan.verble@gmail.com>
2016-06-03 14:13:18 -0700

    move rendering logic to board_graphics.c

--- board_graphics.c
+++ board_graphics.c
@@ -4,8 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glew.h>
+#include "shader.h"
+#include "board_graphics.h"
 
 
+// 2 triangles per square * 3 vertices per triangle
+const int VERTICES_PER_CELL = 2 * 3;
 // 2 triangles per square * 3 vertices per triangle * 2 coords per vertex
 const int POS_SEGMENT_SIZE = 2 * 3 * 2;
 // 2 triangles per square * 3 vertices per triangle * 3 color points per vertex
@@ -91,4 +95,74 @@ GLfloat* generate_col_buffer(Board b) {
     }
 
     return buffer;
+}
+
+
+
+
+// OpenGL must be ready to go
+BoardGraphics board_graphics_create(Board b) {
+
+    BoardGraphics g;
+    g.board = b;
+    g.positions = generate_pos_buffer(b);
+    g.colors = generate_col_buffer(b);
+    g.program = shprog_new("vertex_shader.glsl", "fragment_shader.glsl");
+    g.position_buffer_size =
+        g.board.width * g.board.height * POS_SEGMENT_SIZE * sizeof(GLfloat);
+    g.color_buffer_size =
+        g.board.width * g.board.height * COL_SEGMENT_SIZE * sizeof(GLfloat);
+
+    // vertex array object
+    glGenVertexArrays(1, &(g.vao));
+
+    // vertex buffers
+    glGenBuffers(1, &(g.position_buffer));
+    glGenBuffers(1, &(g.color_buffer));
+
+    // copy data
+    glBindVertexArray(g.vao);
+    glBindBuffer(GL_ARRAY_BUFFER, g.position_buffer);
+    glBufferData(GL_ARRAY_BUFFER, g.position_buffer_size, g.positions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, g.color_buffer);
+    glBufferData(GL_ARRAY_BUFFER, g.color_buffer_size, g.colors, GL_STREAM_DRAW);
+
+    // register attributes
+    glBindBuffer(GL_ARRAY_BUFFER, g.position_buffer);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, g.color_buffer);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), NULL);
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    return g;
+}
+
+void board_graphics_draw(BoardGraphics g) {
+    shprog_use(g.program);
+    glBindVertexArray(g.vao);
+    glDrawArrays(GL_TRIANGLES, 0, (g.board.width * g.board.height * VERTICES_PER_CELL));
+    glBindVertexArray(0);
+}
+
+void board_graphics_update(BoardGraphics* g, Board b) {
+    (*g).board = b;
+    free((*g).colors);
+    (*g).colors = generate_col_buffer(b);
+    glBindBuffer(GL_ARRAY_BUFFER, (*g).color_buffer);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, (*g).color_buffer_size, (*g).colors);
+}
+
+// does NOT free the board
+void board_graphics_destroy(BoardGraphics g) {
+
+    glDeleteVertexArrays(1, &(g.vao));
+    glDeleteBuffers(1, &(g.position_buffer));
+    glDeleteBuffers(1, &(g.color_buffer));
+    glDeleteProgram(g.program);
+
+    free(g.positions);
+    free(g.colors);
 }
--- board_graphics.h
+++ board_graphics.h
@@ -9,4 +9,24 @@ GLfloat* generate_pos_buffer(Board b);
 
 GLfloat* generate_col_buffer(Board b);
 
+typedef struct BoardGraphics {
+    Board board;
+    GLfloat* positions;
+    GLfloat* colors;
+    ShaderProgram program;
+    GLuint vao;
+    GLuint position_buffer;
+    size_t position_buffer_size;
+    GLuint color_buffer;
+    size_t color_buffer_size;
+} BoardGraphics;
+
+void board_graphics_destroy(BoardGraphics g);
+
+void board_graphics_update(BoardGraphics* g, Board b);
+
+void board_graphics_draw(BoardGraphics g);
+
+BoardGraphics board_graphics_create(Board b);
+
 #endif // BOARD_GRAPHICS_H
