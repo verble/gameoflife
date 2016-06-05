012d459c431191a2c3ef375ea1d6771d893dcf6a
Bryan Verble <bryan.verble@gmail.com>
2016-06-04 21:01:49 -0700

    add working graphics functionality

--- Makefile
+++ Makefile
@@ -5,11 +5,20 @@ LINKER_FLAGS = -framework OpenGL -lglfw3 -lglew
 
 all: test
 
-test: window.o
-	clang -o test window.o $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(LINKER_FLAGS)
+test: window.o shader.o board.o board_graphics.o
+	clang -o test window.o shader.o board.o board_graphics.o $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(LINKER_FLAGS)
 
 window.o: window.c
 	clang -c window.c
+
+shader.o: shader.c
+	clang -c shader.c
+
+board.o: board.c
+	clang -c board.c
+
+board_graphics.o: board_graphics.c
+	clang -c board_graphics.c
 
 clean:
 	rm -f test
--- window.c
+++ window.c
@@ -4,15 +4,18 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "shader.h"
+#include "board.h"
+#include "board_graphics.h"
 
 
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-const GLuint WIDTH = 800;
-const GLuint HEIGHT = 600;
+const GLuint WIDTH = 500;
+const GLuint HEIGHT = 500;
 
-int main() {
+GLFWwindow* init_graphics() {
 
     // init GLFW
     glfwInit();
@@ -32,6 +35,53 @@ int main() {
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
+    return window;
+}
+
+void terminate_graphics() {
+
+    glfwTerminate();
+}
+
+void run_window() {
+
+    GLFWwindow* window = init_graphics();
+
+    Board b = board_generate(5, 5, 1, "0000000000011100000000000");
+    GLfloat* positions = generate_pos_buffer(b);
+    GLfloat* colors = generate_col_buffer(b);
+
+    // shader
+    ShaderProgram shprog = shprog_new("vertex_shader.glsl", "fragment_shader.glsl");
+
+    // vertex array object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    // vertex buffers
+    GLuint position_buffer;
+    glGenBuffers(1, &position_buffer);
+
+    GLuint color_buffer;
+    glGenBuffers(1, &color_buffer);
+
+    // copy data
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    glBufferData(GL_ARRAY_BUFFER, (25 * 2 * 3 * 2 * sizeof(GLfloat)), positions, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    glBufferData(GL_ARRAY_BUFFER, (25 * 2 * 3 * 3 * sizeof(GLfloat)), colors, GL_STATIC_DRAW);
+
+    // register attributes
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), NULL);
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
     // graphics loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -39,16 +89,32 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        shprog_use(shprog);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, (25 * 3 * 2));
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
     }
 
-    glfwTerminate();
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &position_buffer);
+    glDeleteBuffers(1, &color_buffer);
 
-    return 0;
+    free(positions);
+    free(colors);
+    board_destroy(b);
+
+    terminate_graphics();
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
+}
+
+int main() {
+    run_window();
+    return 0;
 }
